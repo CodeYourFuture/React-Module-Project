2f30d72165091ad020e48c3be8a9aa259628e5d8
bahador <noor.bahadory786@gmail.com>
2024-02-20 14:37:33 +0000

    changed based on feedback

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,12 @@
 import Bookings from "@/components/Bookings/Bookings.jsx";
 import AppHeader from "../AppHeader/AppHeader.jsx";
-import SpaLog from "@/assets/spa-logo.png";
-import HotelLogo from "@/assets/hotel-logo.png";
+
 import "./App.scss";
 
 const App = () => (
-  <div className="app">
-    <img
-      style={{
-        width: "100px",
-        height: "100px",
-      }}
-      src={HotelLogo}
-      alt="hotel logo"
-    />
+  <div>
     <AppHeader />
     <Bookings />
-    <img src={SpaLog} alt="hotel background picture" />
   </div>
 );
 
--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,8 +1,16 @@
-import Logo from "@/assets/spa-logo.png";
 import "./AppHeader.scss";
+import SpaLog from "@/assets/spa-logo.png";
 
 const AppHeader = () => (
   <header className="app__header">
+    <img
+      style={{
+        width: "150px",
+        height: "150px",
+      }}
+      src={SpaLog}
+      alt="hotel background picture"
+    />
     <h1 className="app__heading">CYF Hotel</h1>
   </header>
 );
