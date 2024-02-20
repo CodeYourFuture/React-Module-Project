c7aa7b16afc661c49b1ed562ac9a7d49f9527aeb
bahador <noor.bahadory786@gmail.com>
2024-02-20 16:04:51 +0000

    created card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,20 @@
-// import "./Card.scss";
+import "./Card.scss";
 
-// const Card = ({ title, url, image }) => {
-//   return (
+const Card = ({ title, url, image }) => {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "red",
+        }}
+        className="card-component"
+      >
+        <h1 style={{ backgroundColor: "green" }}>{title}</h1>
+        <a src={url}></a>
+        <img src={image} alt="picture" />
+      </div>
+    </>
+  );
+};
 
-//   );
-// };
-
-// export default Card;
+export default Card;
