02ee9056363dab5a253ff7d9e8f45cb26b4672d1
Sariat <adenike.sariat@gmail.com>
2024-02-21 20:33:03 +0000

    I created the footer and styled it

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,14 @@
 import Bookings from "@/components/Bookings/Bookings.jsx";
 import "./App.scss";
+import Footer from "@/components/Footer/Footer.jsx";
+import { useState } from "react";
+
+const arr = [
+  "123 Fake Street, London, E1 4UD",
+  "hello@fakehotel.com",
+  "0123 456789",
+];
+
 
 const App = () => (
   <div className="app">
@@ -7,6 +16,7 @@ const App = () => (
       <h1 className="app__heading">CYF Hotel</h1>
     </header>
     <Bookings />
+    <Footer array={arr} />
   </div>
 );
 
