49a44e100688ce5790322ef33155fec48c822557
Marcus <marcus.ats94@gmail.com>
2024-02-22 23:47:33 +0000

    Completed Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,12 @@
-// import "./Card.scss";
+import "./Card.scss";
 
-// const Card = ({ title, url, image }) => {
-//   return (
+const Card = ({ title, url, image }) => {
+  return (
+    <a href={url} className="card">
+      <p className="card__title">{title}</p>
+      <img src={image} className="card__image"></img>
+    </a>
+  );
+};
 
-//   );
-// };
-
-// export default Card;
+export default Card;
