65a07453b8081527c2407e4cfd583c69545c6e1d
Marcus <marcus.ats94@gmail.com>
2024-02-25 00:10:45 +0000

    Fixed issue with loading card's placeholder.svg

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,16 +3,8 @@ import "./Card.scss";
 const Card = ({ title, anchorTag, image }) => {
   return (
     <>
-      <a
-        href={anchorTag}
-        style={{
-          backgroundColor: "red",
-        }}
-        className="card"
-      >
-        <p style={{ backgroundColor: "green" }} className="card__title">
-          {title}
-        </p>
+      <a href={anchorTag} className="card">
+        <p className="card__title">{title}</p>
         <img src={image} className="card__image" alt="picture" />
       </a>
     </>
