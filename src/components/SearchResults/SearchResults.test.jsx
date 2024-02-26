c9655ea9bd1a4aedceca8e3f2acc010de643051e
= <adniyayousaf92@gmail.com>
2024-02-26 20:50:09 +0000

    Added unit testing

--- src/components/SearchResults/SearchResults.test.jsx
+++ src/components/SearchResults/SearchResults.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { describe, it, expect } from "vitest";
+import data from "@/data/fakeBookings.json";
 import SearchResults from '@/components/SearchResults/SearchResults.jsx';
 
 describe("Search Results", () => {
@@ -11,6 +12,19 @@ describe("Search Results", () => {
         render(<SearchResults/>);
         const searchResults = screen.getByRole("table");
         expect(searchResults).toBeInTheDocument();
+      });
+
+      it('should render a table with column for each booking attribute', () => {
+        const { getByTestId, getByText } = render(<SearchResults results={data}/>);
+    
+        expect(getByText('ID')).toBeInTheDocument();
+        expect(getByText('Title')).toBeInTheDocument();
+        expect(getByText('First Name')).toBeInTheDocument();
+        expect(getByText('Sur Name')).toBeInTheDocument();
+        expect(getByText('Email')).toBeInTheDocument();
+        expect(getByText('Room ID')).toBeInTheDocument();
+        expect(getByText('Check In Date')).toBeInTheDocument();
+        expect(getByText('Check Out Date')).toBeInTheDocument();
       });
   });
   
