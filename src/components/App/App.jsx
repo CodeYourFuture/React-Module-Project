6219d16d85be8eab6411f9c7ca133dbb9a1739e9
Sabella-8 <gsabella549@gmail.com>
2024-02-26 21:42:37 +0000

    feature-card

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,11 @@
 import Bookings from "@/components/Bookings/Bookings.jsx";
 import "./App.scss";
-import Deck from "../Deck/Deck";
 const App = () => (
   <div className="app">
     <header className="app__header">
       <h1 className="app__heading">CYF Hotel</h1>
     </header>
     <Bookings />
-    <Deck />
   </div>
 );
 
