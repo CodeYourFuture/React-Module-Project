ba4dc53a2e290059c7def4916853937c63b2e970
= <adniyayousaf92@gmail.com>
2024-02-27 20:40:50 +0000

    Fix the problems in CYF Hotel

--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -4,16 +4,16 @@ import cardsData from "@/data/fakeCards.json";
 
 const Deck = () => {
   // Map over the cardsData array and render a Card component for each card object
-  const data = cardsData.map((card) => (
+  const data = cardsData.map((card,index) => (
     <Card
       title={card.title}
       url={card.url}
       src={card.image}
-      key={crypto.randomUUID()}
+      key={index}
     />
   ));
 
-  return <>{data}</>;
+  return <section className="deck">{data}</section>;
 };
 
 export default Deck;
