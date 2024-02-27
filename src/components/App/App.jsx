f2268afaf020cbf17bab2e3a01b6a192278e588c
Sabella-8 <gsabella549@gmail.com>
2024-02-27 22:28:53 +0000

    MERGINING DECK

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,14 @@
 import Bookings from "@/components/Bookings/Bookings.jsx";
 import "./App.scss";
 import Footer from "../Footer/Footer";
-
+import Deck from "../Deck/Deck";
 const App = () => (
   <div className="app">
     <header className="app__header">
       <h1 className="app__heading">CYF Hotel</h1>
     </header>
     <Bookings />
+    <Deck />
     <Footer
       details={[
         "123 Fake Street, London, E1 4UD",
