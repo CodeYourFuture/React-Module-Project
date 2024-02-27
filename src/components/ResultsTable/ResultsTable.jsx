a3ccf4ef4a1b2a1b7457a8bf1afd5ffef8f3fb60
Elmira Zangeneh <elmira.zangeneh89@gmail.com>
2024-02-27 22:58:04 +0000

    completed the active row feature

--- src/components/ResultsTable/ResultsTable.jsx
+++ src/components/ResultsTable/ResultsTable.jsx
@@ -1,19 +1,35 @@
+import { useState } from "react";
+import "./ResultsTable.scss";
 const ResultsTable = (props) => {
-    return (
-      <>
-        <tr>
-          <td>{props.id}</td>
-          <td>{props.title}</td>
-          <td>{props.firstName}</td>
-          <td>{props.surName}</td>
-          <td>{props.email}</td>
-          <td>{props.roomId}</td>
-          <td>{props.checkInDate}</td>
-          <td>{props.checkOutDate}</td>
-        </tr>
-      </>
-    );
+  const [selectedRows, setSelectedRows] = useState([]);
+  const handleRowClick = (rowId) => {
+    const isSelected = selectedRows.includes(rowId);
+
+    if (isSelected) {
+      setSelectedRows(selectedRows.filter((id) => id !== rowId));
+    } else {
+      setSelectedRows([...selectedRows, rowId]);
+    }
   };
-  
-  export default ResultsTable;
-  
+
+  return (
+    <>
+      <tr
+        role="row"
+        onClick={() => handleRowClick()}
+        className={selectedRows.includes() ? "selected-row" : ""}
+      >
+        <td>{props.id}</td>
+        <td>{props.title}</td>
+        <td>{props.firstName}</td>
+        <td>{props.surName}</td>
+        <td>{props.email}</td>
+        <td>{props.roomId}</td>
+        <td>{props.checkInDate}</td>
+        <td>{props.checkOutDate}</td>
+      </tr>
+    </>
+  );
+};
+
+export default ResultsTable;
