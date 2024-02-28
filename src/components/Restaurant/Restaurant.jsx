73a2593a280291d085b71e59b02a69a6c4aec935
= <adniyayousaf92@gmail.com>
2024-02-28 13:09:40 +0000

    complete the feature

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,14 +1,20 @@
-import "./Restaurant.scss"
+import { useState } from "react";
+import "./Restaurant.scss";
 
 const Restaurant = () => {
-  const pizzas = 0;
+  const [order, setOrder] = useState(0);
   return (
     <section className="restaurant">
       <h3 className="restaurant__heading">Restaurant Orders</h3>
       <ul className="restaurant__list">
-        <li className="restaurant__item">
-          Pizzas: {pizzas}{" "}
-          <button className="button restaurant__button">Add</button>
+        <li data-testid="orders" className="restaurant__item">
+          Pizzas: {order}{" "}
+          <button
+            className="button restaurant__button"
+            // onClick={() => setOrder(order + 1)}
+          >
+            Add
+          </button>
         </li>
       </ul>
     </section>
