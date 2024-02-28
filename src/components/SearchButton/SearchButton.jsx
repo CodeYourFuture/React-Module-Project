4341a8615ad8c9e01c94bd76fcd8e59d46e6e109
Fathi Kahin <fhkahin@gmail.com>
2024-02-28 12:24:43 +0000

    search button completed

--- src/components/SearchButton/SearchButton.jsx
+++ src/components/SearchButton/SearchButton.jsx
@@ -1,1 +1,9 @@
 
+import React from 'react';
+import './SearchButton.scss'; 
+
+const SearchButton = () => (
+  <button className="button search__button">Search</button>
+);
+
+export default SearchButton;
