8fd915611cd6a80a479e8f61c3820a2618e7f26b
Pedram Amani <amaaanip@gmail.com>
2024-02-28 20:17:50 +0000

    Footer added & imported in App.jsx file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,24 @@
+import React from "react";
+import Footer from "@/components/Footer/Footer";
 import Bookings from "@/components/Bookings/Bookings.jsx";
 import "./App.scss";
 
-const App = () => (
-  <div className="app">
-    <header className="app__header">
-      <h1 className="app__heading">CYF Hotel</h1>
-    </header>
-    <Bookings />
-  </div>
-);
+const App = () => {
+  const addresses = [
+    "123 Fake Street, London, E1 4UD",
+    "hello@fakehotel.com",
+    "0123 456789",
+  ];
+
+  return (
+    <div className="app">
+      <header className="app__header">
+        <h1 className="app__heading">CYF Hotel</h1>
+      </header>
+      <Bookings />
+      <Footer addresses={addresses} />
+    </div>
+  );
+};
 
 export default App;
