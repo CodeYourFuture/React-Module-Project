972bbe3f3162ee05cf1ea5acf10b9c06e64b1e68
Your Name <you@example.com>
2024-02-28 21:38:10 +0000

    finished card and deck

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,7 @@
+import React from "react";
 import Bookings from "@/components/Bookings/Bookings.jsx";
 import "./App.scss";
+import Deck from "../Deck/Deck";
 
 const App = () => (
   <div className="app">
@@ -7,6 +9,7 @@ const App = () => (
       <h1 className="app__heading">CYF Hotel</h1>
     </header>
     <Bookings />
+    <Deck/>
   </div>
 );
 
