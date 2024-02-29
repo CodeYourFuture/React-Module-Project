692ab74688238474fd5491c1264bf8dfbb92e06b
RbAvci <rb.avci@gmail.com>
2024-02-29 11:21:35 +0000

    add Card component to app.jsx implemented Card add Css styling for card implemented test for each props of Card components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import Bookings from "@/components/Bookings/Bookings.jsx";
 import "./App.scss";
+import Card from "../Card/Card";
 
 const App = () => (
   <div className="app">
     <header className="app__header">
       <h1 className="app__heading">CYF Hotel</h1>
     </header>
+    <Card title="Welcome to CYF Hotel" url="https://cyf-hotel.com" image="https://cyf-hotel.com/images/banner.jpg" />
     <Bookings />
   </div>
 );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,13 @@
-// import "./Card.scss";
+import "./Card.scss";
 
-// const Card = ({ title, url, image }) => {
-//   return (
+const Card = ({ title, url, image }) => {
+  return (
+    <div className="card">
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <a href={url}>Visit website</a>
+    </div>
+  );
+};
 
-//   );
-// };
-
-// export default Card;
+export default Card;
