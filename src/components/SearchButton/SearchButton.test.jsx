f7777de1738d82b2ae18eca4c756e78e3a124396
Zeliha Pala <zelihapala78@gmail.com>
2024-02-29 20:12:37 +0000

    searchButton tested if it renders in the page

--- src/components/SearchButton/SearchButton.test.jsx
+++ src/components/SearchButton/SearchButton.test.jsx
@@ -3,13 +3,9 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 
 describe("Search Button", () => {
-  it("should have a test, please write one", () => {
-    return true;
+  it("renders a search button", () => {
+    render(<SearchButton />);
+    const searchButton = screen.getByRole("button");
+    expect(searchButton).toBeInTheDocument();
   });
-  // could you start with this?
-  //   it("renders a search button", () => {
-  //     render(<SearchButton />);
-  //     const searchButton = screen.getByRole("button");
-  //     expect(searchButton).toBeInTheDocument();
-  //   });
 });
