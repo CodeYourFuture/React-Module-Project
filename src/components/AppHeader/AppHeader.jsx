eb62a82f460f8ef1e115999043d24590036bc320
Zeliha Pala <zelihapala78@gmail.com>
2024-03-02 15:29:40 +0000

    requested review done

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,11 +1,11 @@
-import Logo from "@/assets/spa-logo.png";
+import Logo from "@/assets/hotel-logo.png";
 import "./AppHeader.scss";
 
 const AppHeader = () => {
     return(
         
     <header className="app__header">
-        <img className="app__logo" src={Logo} alt="" />
+        <img className="app__logo" src={Logo} alt="hotel Logo" />
       <h1 className="app__heading">CYF Hotel</h1>
     </header>
   
