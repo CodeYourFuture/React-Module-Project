2954242fd71115d37b7c479a97609a5a363b7b33
OrlandoMoralesKuan <omoralesk@hotmail.com>
2024-03-03 18:23:49 +0000

    Adding the function restaurant and buttonAdd to increase the number of pizzas when the button Add is clicked

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,18 +1,25 @@
-const Restaurant = () => {
-  const pizzas = 0;
+import { useState } from "react";
+
+function Restaurant() {
+  const [pizzas, setPizzas] = useState(0);
+
+  function buttonAdd() {
+    setPizzas(pizzas + 1);
+  }
+
   return (
     <section className="restaurant">
       <h3 className="restaurant__heading">Restaurant Orders</h3>
       <ul className="restaurant__list">
         <li className="restaurant__item">
           Pizzas: {pizzas}{" "}
-          <button className="button restaurant__button" onClick={Restaurant}>
+          <button className="button restaurant__button" onClick={buttonAdd}>
             Add
           </button>
         </li>
       </ul>
     </section>
   );
-};
+}
 
 export default Restaurant;
