e4fdf30e21c1bfbaaf26f43068be65eb094a6ca7
Pedro Ricciardi <riccip@outlook.com>
2024-03-04 07:47:30 +0000

    fake bookings data assigned to a bookings state

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
+
 import Search from "@/components/Search/Search";
-import FakeBookings from "../../data/fakeBookings.json";
-import SearchResults from "../SearchResults/SearchResults";
+import FakeBookings from "@/data/fakeBookings.json";
+import SearchResults from "@/components/SearchResults/SearchResults";
 
 const Bookings = () => {
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
 
+  const [bookings, setBookings] = useState(FakeBookings);
+
   return (
     <main className="bookings">
       <Search search={search} />
-      {<SearchResults bookings={FakeBookings} />}
+      {<SearchResults bookings={bookings} />}
     </main>
   );
 };
