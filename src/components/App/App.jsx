2a6f3f51ba2725a71d4a18096505697dacff1240
RbAvci <rb.avci@gmail.com>
2024-03-05 09:22:14 +0000

    render restaurant component in App.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,14 @@ import "./App.scss";
 import AppHeader from "../AppHeader/AppHeader.jsx";
 import Card from "../Card/Card";
 import Footer from "../Footer/Footer";
-
-
+import Restaurant from "../Restaurant/Restaurant.jsx";
 
 const App = () => (
   <div className="app">
-    <AppHeader/>
+    <AppHeader />
     <Card title="Welcome to CYF Hotel" url="https://cyf-hotel.com" image="https://cyf-hotel.com/images/banner.jpg" />
     <Bookings />
+    <Restaurant />
     <Footer />
   </div>
 );
