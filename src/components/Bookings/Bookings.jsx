9eb31cf5a7c38519defcbf5bcff2f7d9b4b54703
nazaninsaedi <nazaninsaedi088@gmail.com>
2024-03-05 21:34:08 +0000

    FeatureBookingsStateDone

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,9 +1,15 @@
+import React, { useState } from 'react';
 import Search from "@/components/Search/Search";
 import SearchResult from "../SearchResult/SearchResult";
+import FakeBookings from '@/data/fakeBookings.json';
+
 // import SearchResults from "@/componentsSearchResults.js";
 // import FakeBookings from "@/data/fakeBookings.json";
 
 const Bookings = () => {
+
+  const [bookings, setBookings] = useSatet(FakeBookings);
+
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
   };
@@ -11,7 +17,7 @@ const Bookings = () => {
   return (
     <main className="bookings">
       <Search search={search} />
-      <SearchResult />
+      <SearchResult bookings={bookings} />
     </main>
   );
 };
