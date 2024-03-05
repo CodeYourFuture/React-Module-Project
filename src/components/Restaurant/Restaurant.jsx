01d7ec81e472210a1e3330d7a6fe96cb5b1fa2a9
areebsattar <areebsattar1@gmail.com>
2024-03-05 23:37:59 +0000

    Fixed the name of function

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function Restaurant() {
   const [pizzas, setPizzas] = useState(0);
 
-  function buttonAdd() {
+  function OrderOne() {
     setPizzas(pizzas + 1);
   }
 
@@ -13,7 +13,7 @@ function Restaurant() {
       <ul className="restaurant__list">
         <li className="restaurant__item">
           Pizzas: {pizzas}{" "}
-          <button className="button restaurant__button" onClick={buttonAdd}>
+          <button className="button restaurant__button" onClick={OrderOne}>
             Add
           </button>
         </li>
