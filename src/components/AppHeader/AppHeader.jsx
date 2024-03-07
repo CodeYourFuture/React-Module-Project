b50d478c41715263183830b7f1c218602a61f731
areebsattar <areebsattar1@gmail.com>
2024-03-07 23:05:51 +0000

    Fixed the wrong hotel logo

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,4 +1,4 @@
-import Logo from "@/assets/spa-logo.png";
+import Logo from "@/assets/hotel-logo.png";
 import "./AppHeader.scss";
 const AppHeader = () => <img className="LogoImg" src={Logo}></img>;
 export default AppHeader;
