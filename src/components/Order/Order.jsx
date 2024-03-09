a382a9355667fed0d1d0411c21961f3d388c41dc
fikretellek <fikretellek@icloud.com>
2024-03-09 01:02:25 +0000

    useState and handleAddOrder moved to order.jsx file

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,6 +1,12 @@
 import RestaurantButton from "../RestaurantButton/RestaurantButton";
+import React, { useState } from "react";
 
-export default function Order({ orders, handleAddOrder }) {
+export default function Order() {
+  const [orders, setOrders] = useState(0);
+  const handleAddOrder = () => {
+    setOrders(orders + 1);
+  };
+
   return (
     <li className="restaurant__item">
       Orders: {orders}
--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,17 +1,11 @@
-import React, { useState } from "react";
 import Order from "../Order/Order";
 
 const Restaurant = () => {
-  const [orders, setOrders] = useState(0);
-  const handleAddOrder = () => {
-    setOrders(orders + 1);
-  };
-
   return (
     <section className="restaurant">
       <h3 className="restaurant__heading">Restaurant Orders</h3>
       <ul className="restaurant__list">
-        <Order orders={orders} handleAddOrder={handleAddOrder} />
+        <Order />
       </ul>
     </section>
   );
