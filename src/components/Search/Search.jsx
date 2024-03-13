dfde96da326684d2798705e094f748822738efc3
Sabella-8 <gsabella549@gmail.com>
2024-03-13 01:29:14 +0000

    feature search state

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,24 +1,37 @@
 import "./Search.scss";
 import SearchButton from "../SearchButton/SearchButton";
+import { useState } from "react";
 
-const Search = () => (
-  <section className="search">
-    <header className="search__header">
-      <h4 className="search__heading heading">Search Bookings</h4>
-    </header>
-    <form className="search__form">
-      <label className="search__label" htmlFor="customerName">
-        &rarr;
-      </label>
-      <input
-        type="search"
-        id="customerName"
-        className="search__input"
-        placeholder="Customer name"
-      />
-      <SearchButton />
-    </form>
-  </section>
-);
+const Search = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearchInput = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  console.log(searchInput);
+
+  return (
+    <section className="search">
+      <header className="search__header">
+        <h4 className="search__heading heading">Search Bookings</h4>
+      </header>
+      <form className="search__form">
+        <label className="search__label" htmlFor="customerName">
+          &rarr;
+        </label>
+        <input
+          type="search"
+          id="customerName"
+          className="search__input"
+          placeholder="Customer name"
+          value={searchInput}
+          onChange={handleSearchInput}
+        />
+        <SearchButton />
+      </form>
+    </section>
+  );
+};
 
 export default Search;
