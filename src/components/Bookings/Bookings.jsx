09e80956a1f120c011bb66912882a76522d0691a
Zeliha Pala <zelihapala78@gmail.com>
2024-03-13 02:09:01 +0000

    Updated according to the AppBookingForm Component

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Search from "../Search/Search";
 import FakeBookings from "../../data/fakeBookings.json";
 import SearchResults from "../SearchResults/SearchResults";
+import AddBookingForm from "./AddBookingForm";
 
 const Bookings = () => {
   
@@ -11,11 +12,16 @@ const Bookings = () => {
     console.info("TO DO!", searchVal);
   };
 
+  
+
+
+
   return (
     <main className="bookings">
       <Search search={search} />
 
       <SearchResults results={bookings} />
+      <AddBookingForm bookings={bookings} setBookings={setBookings} />
     </main>
   );
 };
