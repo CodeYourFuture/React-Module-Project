368a5c6715b4c86dc6f86df49e9e2635a1761819
Zeliha Pala <zelihapala78@gmail.com>
2024-03-13 02:07:24 +0000

    form added to AddBookingForm component

---
+++ src/components/Bookings/AddBookingForm.jsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+
+const AddBookingForm = ({ bookings, setBookings }) => {
+  const [formData, setFormData] = useState({
+    id: "",
+    firstName: "",
+    surname: "",
+    email: "",
+    title: "",
+    roomId: "",
+    checkInDate: "",
+    checkOutDate: "",
+  });
+
+  const [nextId, setNextId] = useState(bookings.length + 1);
+ 
+
+
+  const addBooking = (newBooking) => {
+    setBookings([...bookings, newBooking]);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBooking = { ...formData, id: nextId };
+    addBooking(newBooking);
+    setFormData({
+      id: "",
+      firstName: "",
+      surname: "",
+      email: "",
+      title: "",
+      roomId: "",
+      checkInDate: "",
+      checkOutDate: "",
+    });
+    setNextId(nextId + 1);
+   
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        placeholder="First Name"
+        value={formData.firstName}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="surname"
+        placeholder="Surname"
+        value={formData.surname}
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        value={formData.title}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="roomId"
+        placeholder="Room ID"
+        value={formData.roomId}
+        onChange={handleChange}
+      />
+      <input
+        type="date"
+        name="checkInDate"
+        placeholder="Check In Date"
+        value={formData.checkInDate}
+        onChange={handleChange}
+      />
+      <input
+        type="date"
+        name="checkOutDate"
+        placeholder="Check Out Date"
+        value={formData.checkOutDate}
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default AddBookingForm;
